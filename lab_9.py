b07808622081a554a95057e570686ec461c04a49
Aradhana Hegde <aradhanahegde@ufl.edu>
2024-04-10 19:42:47 -0400

    with decoded function

--- lab_9.py
+++ lab_9.py
@@ -5,7 +5,7 @@ def encode(password):
         encoded_password = f"{encoded_password}{char}"
     return encoded_password
 
-def decode(password):
+def decode(encoded_password):
     decoded_password = ""
     for digit in encoded_password:
 	decoded_digit = (int(digit) - 3) % 10
